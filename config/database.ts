971cb36c3b2046599a4e16405c4d45e86254b356
silas <nguyengiangthanhtai2016@gmail.com>
2025-05-23 19:43:17 +0700

    Cập nhật kết nối Database Online

--- config/database.ts
+++ config/database.ts
@@ -1,11 +1,19 @@
 import { Sequelize } from 'sequelize';
+const fs = require('fs');
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USERNAME,
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
     dialect: 'mysql',
+    dialectOptions: {
+      ssl: {
+        ca: fs.readFileSync(__dirname + '/certs/ca.pem'), // bạn phải lưu file `ca.pem` tại đây
+      },
+    },
+    logging: false,
   }
 );
 
