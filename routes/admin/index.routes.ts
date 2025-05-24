38174020cf68df21a6d9e974d7bf14e53265c5f8
NgKhao <bsbnk141@gmail.com>
2025-05-25 01:31:14 +0700

    build UI admin profile

--- routes/admin/index.routes.ts
+++ routes/admin/index.routes.ts
@@ -1,8 +1,10 @@
 import { Express } from 'express';
 import { dashboardRoute } from './dashboard.route';
 import { systemConfig } from '../../config/system';
+import { profileRoute } from './adminProfile.route';
 
 export const routesAdmin = (app: Express) => {
   const path = systemConfig.prefixAdmin;
   app.use(`/${path}/dashboard`, dashboardRoute);
+  app.use(`/${path}/profile`, profileRoute);
 };
