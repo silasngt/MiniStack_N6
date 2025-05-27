095973346341766d03a2e1ddaa2f2456a0ee7027
nghia <2251120098.ut.edu.vn>
2025-05-27 07:51:37 +0700

    Nghia/UserProfile-adminDocument

--- routes/client/index.routes.ts
+++ routes/client/index.routes.ts
@@ -4,11 +4,14 @@ import { forumTopicRoute } from './forumTopic.route';
 import { blogRoute } from './blog.route';
 import { authRoute } from './auth.route';
 import { UserProfileRoute } from './UserProfile.route';
+import { historyRoute } from './history.route';
 
 export const routesClient = (app: Express) => {
   app.use('/', miniStackRoute);
   app.use('/forum-topic', forumTopicRoute);
   app.use('/blog', blogRoute);
   app.use('/auth', authRoute);
-  app.use ('/UserProfile',UserProfileRoute)
+  app.use ('/UserProfile',UserProfileRoute);
+  app.use ('/history',historyRoute)
+
 };
