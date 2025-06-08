f3bb42756015d26dd9d28826bca8bf39026a639c
silas <nguyengiangthanhtai2016@gmail.com>
2025-06-08 22:52:36 +0700

    BE cho trang dashboard và Index và Blog

--- controllers/admin/testCloud.controller.ts
+++ controllers/admin/testCloud.controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express';
 import TestCloud from '../../models/testCloud.model';
+import { generateRandomString } from '../../helpers/generate.helper';
+import md5 from 'md5';
 
 export const index = async (req: Request, res: Response) => {
   res.render('admin/pages/testCloud/index.pug', {
@@ -8,12 +10,12 @@ export const index = async (req: Request, res: Response) => {
 };
 
 export const postImage = async (req: Request, res: Response) => {
-  // console.log(req.body);
+  console.log(req.body);
 
-  const data = {
-    testCloudimage: req.body.testCloud || '',
-  };
-  await TestCloud.create(data);
+  // const data = {
+  //   testCloudimage: req.body.testCloud || '',
+  // };
+  // await TestCloud.create(data);
 
   res.render('admin/pages/testCloud/index.pug', {
     pageTitle: 'Test Cloud',
@@ -22,12 +24,12 @@ export const postImage = async (req: Request, res: Response) => {
 };
 
 export const imageField = async (req: Request, res: Response) => {
-  // console.log(req.body);
-  const data = {
-    testCloudimage: JSON.stringify(req.body.testCloudMultiple || []),
-  };
+  console.log(req.body);
+  // const data = {
+  //   testCloudimage: JSON.stringify(req.body.testCloudMultiple || []),
+  // };
 
-  await TestCloud.create(data);
+  // await TestCloud.create(data);
   const imageFields = req.body.testCloudMultiple || [];
   res.render('admin/pages/testCloud/index.pug', {
     pageTitle: 'Test Cloud',
--- routes/client/blog.route.ts
+++ routes/client/blog.route.ts
@@ -2,7 +2,7 @@ import express from 'express';
 const router = express.Router();
 
 import * as controller from '../../controllers/client/blog.controller';
-
 router.get('/', controller.index);
+router.get('/:id', controller.detailBlog);
 
 export const blogRoute = router;
